c82ad786ae921ce60acbcdb8c2b7f22e1343cb17
FraGoTe <Francis-gonzales@hotmail.com>
2012-05-28 04:10:42 -0500

    Eliminando archivos desfasados

--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -3,45 +3,79 @@
 class LoginController extends Zend_Controller_Action
 {
 
-     public function init()
+     public function getForm()
     {
-        $username = $this->addElement('text', 'username', array(
-            'filters'    => array('StringTrim', 'StringToLower'),
-            'validators' => array(
-                'Alpha',
-                array('StringLength', false, array(3, 20)),
-            ),
-            'required'   => true,
-            'label'      => 'Your username:',
-        ));
-
-        $password = $this->addElement('password', 'password', array(
-            'filters'    => array('StringTrim'),
-            'validators' => array(
-                'Alnum',
-                array('StringLength', false, array(6, 20)),
-            ),
-            'required'   => true,
-            'label'      => 'Password:',
-        ));
-
-        $login = $this->addElement('submit', 'login', array(
-            'required' => false,
-            'ignore'   => true,
-            'label'    => 'Login',
-        ));
-
-        // We want to display a 'failed authentication' message if necessary;
-        // we'll do that with the form 'description', so we need to add that
-        // decorator.
-        $this->setDecorators(array(
-            'FormElements',
-            array('HtmlTag', array('tag' => 'dl', 'class' => 'zend_form')),
-            array('Description', array('placement' => 'prepend')),
-            'Form'
+        return new Application_Form_Login(array(
+            'action' => '/login/process',
+            'method' => 'post',
         ));
     }
-    
 
+    public function getAuthAdapter(array $params)
+    {
+        // Leaving this to the developer...
+        // Makes the assumption that the constructor takes an array of 
+        // parameters which it then uses as credentials to verify identity.
+        // Our form, of course, will just pass the parameters 'username'
+        // and 'password'.
+    }
+    
+     public function preDispatch()
+    { 
+        if (Zend_Auth::getInstance()->hasIdentity()) {
+            // If the user is logged in, we don't want to show the login form;
+            // however, the logout action should still be available
+            if ('logout' != $this->getRequest()->getActionName()) {
+                $this->_helper->redirector('index', 'index');
+            }
+        } else {
+            // If they aren't, they can't logout, so that action should 
+            // redirect to the login form
+            if ('logout' == $this->getRequest()->getActionName()) {
+                $this->_helper->redirector('index');
+            }
+        }
+    }
+    public function indexAction()
+    {        
+        $this->view->form = $this->getForm();
+    }
+    public function processAction()
+    {
+        $request = $this->getRequest();
+
+        // Check if we have a POST request
+        if (!$request->isPost()) {
+            return $this->_helper->redirector('index');
+        }
+
+        // Get our form and validate it
+        $form = $this->getForm();
+        if (!$form->isValid($request->getPost())) {
+            // Invalid entries
+            $this->view->form = $form;
+            return $this->render('index'); // re-render the login form
+        }
+
+        // Get our authentication adapter and check credentials
+        $adapter = $this->getAuthAdapter($form->getValues());
+        $auth    = Zend_Auth::getInstance();
+        $result  = $auth->authenticate($adapter);
+        if (!$result->isValid()) {
+            // Invalid credentials
+            $form->setDescription('Invalid credentials provided');
+            $this->view->form = $form;
+            return $this->render('index'); // re-render the login form
+        }
+
+        // We're authenticated! Redirect to the home page
+        $this->_helper->redirector('index', 'index');
+    }
+     public function logoutAction()
+    {
+        Zend_Auth::getInstance()->clearIdentity();
+        $this->_helper->redirector('index'); // back to login page
+    }
+  
 }
 
