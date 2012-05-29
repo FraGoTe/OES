ef4ccfe41dda8affe39ac1efdb5be919a6761de4
FraGoTe <Francis-gonzales@hotmail.com>
2012-05-29 13:49:37 -0500

    avance de el init view

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,9 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
 
-    //Zend Forms
+    private $_acl = null;
+
+        //Zend Forms
     protected function _initView() {
         //Inicializamos la vista
         $view = new Zend_View();
@@ -18,5 +20,16 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
 
         return $view;
     }
-
+    //Inicializando las opciones
+    function _initViewHelpers(){
+        
+        $this->bootstrap('layout');
+        $layout = $this->getResource('layout');
+        $view = $layout->getView();
+        
+        $navContainerConfig = new Zend_Config_Xml(APPLICATION_PATH.'/config/navigation.xml','nav');
+        $navContainer = new Zend_Navigation($navContainerConfig);
+        
+        $view->navigation($navContainer);
+    }
 }
