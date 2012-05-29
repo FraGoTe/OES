0c45eb9d9eebd4c7eddd6e477975fc398da4368f
FraGoTe <Francis-gonzales@hotmail.com>
2012-05-29 13:15:37 -0500

    creando la opcion de datos del alumno

---
+++ application/controllers/DatosAlumnoController.php
@@ -0,0 +1,25 @@
+<?php
+
+class DatosAlumnoController extends Zend_Controller_Action
+{
+
+    public function preDispatch() {
+        $auth = Zend_Auth::getInstance();
+        if (!$auth->hasIdentity()) {
+            $this->_redirect('login');
+        }
+    }
+    
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+
+}
+
