5b471d5b6b3f936b73891b04f678f553c8c48ee6
FraGoTe <Francis-gonzales@hotmail.com>
2012-05-29 12:50:06 -0500

    creando el formulario de Login

--- application/forms/Login.php
+++ application/forms/Login.php
@@ -9,31 +9,28 @@ class Application_Form_Login extends Zend_Form
             'filters'    => array('StringTrim', 'StringToLower'),
             'validators' => array(
                 'Alpha',
-                array('StringLength', false, array(3, 20)),
+                array('StringLength', true, array(3, 20)),
             ),
             'required'   => true,
-            'label'      => 'Your username:',
+            'label'      => 'Usuario:',
         ));
 
         $password = $this->addElement('password', 'password', array(
             'filters'    => array('StringTrim'),
             'validators' => array(
                 'Alnum',
-                array('StringLength', false, array(6, 20)),
+                array('StringLength', true, array(6, 20)),
             ),
             'required'   => true,
             'label'      => 'Password:',
         ));
-
+       
         $login = $this->addElement('submit', 'login', array(
             'required' => false,
             'ignore'   => true,
             'label'    => 'Login',
         ));
 
-        // We want to display a 'failed authentication' message if necessary;
-        // we'll do that with the form 'description', so we need to add that
-        // decorator.
         $this->setDecorators(array(
             'FormElements',
             array('HtmlTag', array('tag' => 'dl', 'class' => 'zend_form')),
