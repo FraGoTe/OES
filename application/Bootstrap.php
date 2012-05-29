7dd6730b5a26f3dc193e30477c9d7311d919ad30
FraGoTe <Francis-gonzales@hotmail.com>
2012-05-29 12:49:17 -0500

    Inicializando la vista en el bootstrap

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,22 +1,22 @@
 <?php
 
-class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
-{
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
+
     //Zend Forms
     protected function _initView() {
-        
         //Inicializamos la vista
         $view = new Zend_View();
         $view->setEncoding('UTF-8');
         $view->doctype('XHTML1_STRICT');
         $view->headMeta()
-             ->appendHttpEquiv('Content-Type','text/html;charset=utf-8');
+                ->appendHttpEquiv('Content-Type', 'text/html;charset=utf-8');
 
         //renderizamos la vista
-        $viewRenderer = 
-        Zend_Controller_Action_HelperBroker::getStaticHelper('ViewRenderer');
+        $viewRenderer =
+                Zend_Controller_Action_HelperBroker::getStaticHelper('ViewRenderer');
         $viewRenderer->setView($view);
-        
+
         return $view;
     }
+
 }
