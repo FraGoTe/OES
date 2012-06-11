3dd085a7554da72f71bcdd4b9e05ec3a6b6f97d1
Francis Gonzales <Francis-Gonzales@hotmail.com>
2012-06-11 02:56:19 -0500

    Modificando la llamada al proceso de validacion de login

--- Implementation/Login/ProcessLogin.php
+++ Implementation/Login/ProcessLogin.php
@@ -1,5 +1,5 @@
 <?php
-include '../Controllers/LoginController.php';
+include '../../Controllers/LoginController.php';
 
 $ObjLogin = new Login();
 $retorno = $ObjLogin->valida($_POST['username'], $_POST['password']);
