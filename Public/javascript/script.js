53a3ee27e5e3c0f27ed1fabd1b21f3c2f0b33a09
Francis Gonzales <francis-gonzales@hotmail.com>
2012-07-15 16:23:49 -0500

    cambios

---
+++ Public/javascript/script.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function fechaLargaHoy(){
+    var fechaActual = new Date();
+    var diasemana = fechaActual.getDay();
+    var dia = fechaActual.getDate();
+    var mes = fechaActual.getMonth() +1;
+    var ano = fechaActual.getFullYear();
+    var nomMes=nameMes(mes);
+    var nomDia=nameDia(diasemana);
+    var fecha = nomDia+" "+dia+" de "+nomMes+" del "+ano;
+    $('#fecha').html(fecha);
+}
+
