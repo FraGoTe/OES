8b3f203eb641ed0eacdd65eb4e452721d5cd4f33
unknown <Peter@peter-439b2b1d0.(none)>
2012-07-17 18:29:09 -0500

    agregados terminos y condiciones

--- Views/selecciona-cursos.php
+++ Views/selecciona-cursos.php
@@ -28,11 +28,17 @@ include "{$_SERVER['DOCUMENT_ROOT']}/Implementation/Selecciona-Cursos/ObtenerCur
 </fieldset>
 <div class="modal hide fade" id="myModal">
             <div class="modal-header">
-              <h3>T&eacuterminos y condiciones de la Matr&iacutecula</h3>
+              <h3>ATENCI&Oacute;N</h3>
             </div>
             <div class="modal-body">
-              <h4>Atenci&oacute;n</h4>
-              <p>Recuerde que esta matricula no tiene valor oficial.</p>
+                <h4>T&eacuterminos y condiciones de la Matr&iacutecula</h4><p></p>
+              <p>- El alumno se hace responsable de toda afirmación realizada o acto ocurrido mediante el uso de su nombre de usuario y contraseña.</p>
+              <p>- EL alumno es el único responsable por los cursos elegidos y por toda informacion brindada para el proceo de matricula online.</p>
+              <p>- La FIEI se reserva el derecho de eliminar o modificar cualquier información, comunicación, material de descarga o mensaje que en su opinión viole políticas de uso.</p>
+              <p>- La FIEI se hace responsable por la privacidad de la información veraz entregada por el Usuario, no revelará, ni compartirá esta información sin el consentimiento del usuario, excepto cuando lo requiera la ley o a solicitud del gobierno</p>
+              <p>- Al aceptar los terminos y condiciones, asegura que está de acuerdo con todo lo estipulado anteriormente.</p>
+
+
             </div>
             <div class="modal-footer">
                 <a data-dismiss="modal" class="btn" onclick="$('#myModal').modal('hide');" href="#">Cancelar</a>
