3185aeb260f11d6dc98183bfe0502746d4e6609c
unknown <Peter@peter-439b2b1d0.(none)>
2012-07-17 19:00:36 -0500

    manual del estudiante

--- Views/manual-estudiante.php
+++ Views/manual-estudiante.php
@@ -2,7 +2,7 @@
 <input type="hidden" name="docid" id="docid" value="0008" />
 <input type="hidden" name="intercod" id="intercod" value="0008" />
 <object  width="700" height="530"> 
-    <embed src="../Public/Documentos/escuela_informatica_malla_ curricular.pdf" width="670" height="700" type="application/pdf">
+    <embed src="../Public/Documentos/manual_estudiante.pdf" width="670" height="700" type="application/pdf">
         
         </embed>
 </object>
