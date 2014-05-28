f81d92f20e96a4396b7e15eaecb2604c31e848ab
Francis Gonzales <fgonzalestello91@gmail.com>
2014-05-28 09:00:28 -0500

    Update AdminEstadoMatriculaController.php

--- Controllers/AdminEstadoMatriculaController.php
+++ Controllers/AdminEstadoMatriculaController.php
@@ -4,47 +4,57 @@ include_once "{$_SERVER['DOCUMENT_ROOT']}./Models/Alumno.php";
 
 class AdminEstadoMatriculaController {
     
-    function __construct() {
+    function __construct() 
+    {
         
     }
-     public function getalldata($variable){
-           $objalumno = new Alumno();
-           $data1 = $objalumno->getAllAluxEscuela($variable);
-           return $data1;
-        }
+    
+    public function getalldata($variable)
+    {
+        $objalumno = new Alumno();
+        $data1 = $objalumno->getAllAluxEscuela($variable);
+ 
+        return $data1;
+    }
         
          
-       public function getuserdata($variable,$start,$limit,$sidx,$sord){
-           $objalumno = new Alumno();
-           $data1 = $objalumno->getAluxEscuela($variable,$start,$limit,$sidx,$sord);
-           return $data1;
-        }
+    public function getuserdata($variable,$start,$limit,$sidx,$sord){
+        $objalumno = new Alumno();
+        $data1 = $objalumno->getAluxEscuela($variable,$start,$limit,$sidx,$sord);
+ 
+        return $data1;
+    }
+    
+    public function getcursos($variable)
+    {
+        $objalumno = new Alumno();
+        $data1 = $objalumno->getAllCursos($variable);
+ 
+        return $data1;
+    }
+    
+    public function getuserestado($variable)
+    {
+        $objalumno = new Alumno();
+        $data2 = $objalumno->getestado($variable);
+ 
+        return $data2;
+    }
+    
+    public function getaludata($datos=array(),$escuela)
+    {
+        $objalumno = new Alumno();
+        $data1 = $objalumno->getAllAluFiltro($datos,$escuela);
         
-       public function getcursos($variable){
-           $objalumno = new Alumno();
-           $data1 = $objalumno->getAllCursos($variable);
-           return $data1;
-        }
+        return $data1;
+    }
+    
+    public function getaludata2($datos=array())
+    {
+        $objalumno = new Alumno();
+        $data1 = $objalumno->getAllAluFiltro2($datos);
         
-        public function getuserestado($variable){
-           $objalumno = new Alumno();
-           $data2 = $objalumno->getestado($variable);
-           return $data2;
-        }
-        public function getaludata($datos=array(),$escuela)
-        {
-           $objalumno = new Alumno();
-           $data1 = $objalumno->getAllAluFiltro($datos,$escuela);
-           return $data1;
-        }
-        public function getaludata2($datos=array())
-        {
-           $objalumno = new Alumno();
-           $data1 = $objalumno->getAllAluFiltro2($datos);
-           return $data1;
-        }
-
-
+        return $data1;
+    }
 }
-
 ?>
