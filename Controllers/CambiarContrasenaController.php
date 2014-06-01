bc46bbde32ac20ed0fee682725ecafa11a39a5ea
Francis Gonzales <fgonzalestello91@gmail.com>
2014-05-31 22:09:48 -0500

    Update CambiarContrasenaController.php

--- Controllers/CambiarContrasenaController.php
+++ Controllers/CambiarContrasenaController.php
@@ -4,5 +4,6 @@ include_once "{$_SERVER['DOCUMENT_ROOT']}/Models/Usuario.php";
 
     session_start(); 
     $ObjUsuario = new Usuario();
+    
     echo $ObjUsuario->actualizarPassword($_SESSION['usucod'], $_SESSION['usupass'], $_REQUEST['passactual'], $_REQUEST['passnew1'], $_REQUEST['passnew2']);
 ?>
