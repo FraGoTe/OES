8adf85ae090341c2830a37cec027cee706133eef
Francis Gonzales <fgonzalestello91@gmail.com>
2014-06-07 22:04:19 -0500

    Update Valida.php

--- Implementation/Valida-Matricula/Valida.php
+++ Implementation/Valida-Matricula/Valida.php
@@ -3,10 +3,6 @@
 include "{$_SERVER['DOCUMENT_ROOT']}/Controllers/ValidaMatriculaController.php";
 
 session_start();
-
 $objma = new ValidaMatricula();
-
 echo $objma->Valida($_SESSION['alu_cod']);
-
-
 ?>
